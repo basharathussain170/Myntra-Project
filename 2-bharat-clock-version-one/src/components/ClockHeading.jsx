d242077570fe8f9e0f744053fd8e16b921a72e8f
Basharat Hussain <basharathussain0170@gmail.com>
2026-06-29 13:33:26 +0530

    Final Commit

--- 2-bharat-clock-version-one/src/components/ClockHeading.jsx
+++
@@ -1,4 +0,0 @@
-function ClockHeading(){
-  return <h1 className="fw-bolder">Bharat Clock</h1>
-}
-export default ClockHeading
--- 2-bharat-clock-version-one/src/components/CurrentTime.jsx
+++
@@ -1,5 +0,0 @@
-function CurrentTime(){
-  let date=new Date()
-  return <p className="lead">This is current time:{date.toLocaleDateString()} - {date.toLocaleTimeString()}</p>
-}
-export default CurrentTime
